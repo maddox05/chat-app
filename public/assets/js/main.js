89df04a7828f5d60f87a691037e7b323c806fba2
maddox05 <maddoxpublic@gmail.com>
2023-09-02 12:51:51 -0400

    monocraft font

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,4 +1,4 @@
-import { firebaseConfig } from "/firebase_secret.js"; // config from firebase_secret.js
+import { firebaseConfig } from "/public/firebase_secret.js"; // config from firebase_secret.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-app.js"; // must always init firebase app
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-firestore.js"; // add the firestore new database
 import { getAuth, GoogleAuthProvider, signInWithPopup }  from "https://www.gstatic.com/firebasejs/9.17.1/firebase-auth.js"; // add the auth
@@ -14,12 +14,16 @@ const analytics = getAnalytics(app); // init the analytics
 console.log("firebase succesfully started"); // log to console
 
 
+const sign_in_button = document.getElementById("signin"); // get the sign-in button
+const username_div = document.getElementById("username"); // get the username div
+
+
 function sign_in() {
     const provider = new GoogleAuthProvider(); // init the provider
     signInWithPopup(auth, provider) // promise func to sign in
         .then((result) => { // if success .then(result, func)
             const user = result.user;
-            console.log('User signed in:', user.displayName);
+            username_div.innerHTML = ('User signed in:', user.displayName);
             // redirect user / open chat room
         })
         .catch((error) => { // if error .catch(error, func)
@@ -29,7 +33,6 @@ function sign_in() {
         })
 
 }
-const sign_in_button = document.getElementById("signin"); // get the sign-in button
 
 sign_in_button.addEventListener("click", sign_in);
 
